54533ac821feba519a77d119e631cf6a1e3f0e57
Oleg Platonov <platonovrf@bk.ru>
2021-12-13 00:53:49 +0300

    Добавьте файлы проекта.

---
+++ OpenCVproject3/Extention.cs
@@ -0,0 +1,22 @@
+﻿using OpenCvSharp;
+
+namespace Lab_03
+{
+    public static class Extention
+    {
+        public static Mat GrayOtsu(Mat image)
+        {
+            Mat gray = new(image.Rows, image.Cols, MatType.CV_8UC1);
+            Cv2.CvtColor(image, gray, ColorConversionCodes.RGB2GRAY);
+
+            return gray.Threshold(0, 255, ThresholdTypes.Otsu);
+        }
+
+        public static void ShowImage(Mat image, string message)
+        {
+            Cv2.ImShow(message, image);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
---
+++ OpenCVproject3/Program.cs
@@ -0,0 +1,95 @@
+﻿using OpenCvSharp;
+
+namespace Lab_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dilate();
+            Erosion();
+            Close();
+            Open();
+            Skeleton();
+        }
+
+        static void Erosion()
+        {
+            Mat image = Cv2.ImRead("pic.jpg");
+
+            var binary = Extention.GrayOtsu(image);
+            Extention.ShowImage(binary, "gray to binary");
+
+            Cv2.BitwiseNot(binary, binary);
+            Cv2.Erode(binary, binary, new Mat());
+            Cv2.BitwiseNot(binary, binary);
+            Extention.ShowImage(binary, "Erode");
+        }
+
+        static void Dilate()
+        {
+            Mat image = Cv2.ImRead("pic.jpg");
+
+            Mat binary = Extention.GrayOtsu(image);
+            Extention.ShowImage(binary, "gray to binary");
+
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+            Cv2.Dilate(binary, binary, kernel);
+            Extention.ShowImage(binary, "Dilate");
+        }
+
+        static void Close()
+        {
+            Mat image = Cv2.ImRead("pic.jpg");
+
+            Mat binary = Extention.GrayOtsu(image);
+            Extention.ShowImage(binary, "gray to binary");
+
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+            Cv2.MorphologyEx(binary, binary, MorphTypes.Close, kernel);
+            Extention.ShowImage(binary, "Closing");
+        }
+
+        static void Open()
+        {
+            Mat image = Cv2.ImRead("pic.jpg");
+
+            Mat binary = Extention.GrayOtsu(image);
+            Extention.ShowImage(binary, "gray to binary");
+
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+            Cv2.MorphologyEx(binary, binary, MorphTypes.Open, kernel);
+            Extention.ShowImage(binary, "Opening");
+        }
+
+        static void Skeleton()
+        {
+            Mat image = Cv2.ImRead("pic.jpg");
+
+            Mat binary = Extention.GrayOtsu(image);
+            Extention.ShowImage(binary, "gray to binary");
+
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new Size(3, 3));
+            Mat eroded = new();
+            Mat temp = new();
+            Mat skel = new(binary.Rows, binary.Cols, MatType.CV_8UC1, new Scalar(0));
+
+            int size = image.Rows * image.Cols;
+            bool done = false;
+            while (!done)
+            {
+                Cv2.MorphologyEx(binary, eroded, MorphTypes.Erode, kernel);
+                Cv2.MorphologyEx(eroded, temp, MorphTypes.Dilate, kernel);
+                Cv2.Subtract(binary, temp, temp);
+                Cv2.BitwiseOr(skel, temp, skel);
+                eroded.CopyTo(binary);
+
+                int zeros = size - Cv2.CountNonZero(binary);
+                if (zeros == size)
+                    done = true;
+            }
+
+            Extention.ShowImage(skel, "Skeleton");
+        }
+    }
+}
